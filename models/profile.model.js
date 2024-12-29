b0f6cba060eb1c83b0ba65448d2f38ed9df7e032
pandey88himanshu <pandey88himanshu@gmail.com>
2024-12-29 12:29:37 +0530

    profile model created

---
+++ models/profile.model.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const profileSchema = new mongoose.Schema(
+  {
+    gender: {
+      type: String,
+      //   enum: ["Male", "Female", "Other"],
+      trim: true,
+    },
+    dateOfBirth: {
+      type: Date,
+      required: [true, "Date of Birth is required"],
+    },
+    about: {
+      type: String,
+      trim: true,
+    },
+    contactNumber: {
+      type: String,
+      trim: true,
+      validate: {
+        validator: function (value) {
+          return /^[+]?[0-9]{10,15}$/.test(value);
+        },
+        message: "Please enter a valid contact number",
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Profile", profileSchema);
