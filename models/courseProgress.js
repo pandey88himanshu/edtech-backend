89b92140505e92aa38156b1e1148299f6144e01c
pandey88himanshu <pandey88himanshu@gmail.com>
2024-12-29 12:35:48 +0530

    course progress model created

---
+++ models/courseProgress.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const courseProgressSchema = new mongoose.Schema(
+  {
+    courseID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+      required: true,
+    },
+    completedVideos: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "SubSection",
+        required: true,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("CourseProgress", courseProgressSchema);
