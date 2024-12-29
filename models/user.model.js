c5fb2c9ea5e2a48276f57f06137227a1d6adb8d9
pandey88himanshu <pandey88himanshu@gmail.com>
2024-12-29 12:19:08 +0530

    updated user model

--- models/user.model.js
+++ models/user.model.js
@@ -16,13 +16,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: [true, "Email is required"],
       trim: true,
-      unique: true,
       lowercase: true,
     },
     password: {
       type: String,
       required: [true, "Password is required"],
-      minlength: [6, "Password must be at least 6 characters long"],
     },
     accountType: {
       type: String,
